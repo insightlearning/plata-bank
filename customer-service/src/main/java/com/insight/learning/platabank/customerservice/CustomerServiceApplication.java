b4fff65b66913405fccfae7a3cc79a5e4cb9819e
Diego Lucas <diego.lucasilva@gmail.com>
2019-10-18 16:08:07 -0300

    chore: new microservice init

---
+++ customer-service/src/main/java/com/insight/learning/platabank/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.insight.learning.platabank.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
---
+++ customer-service/src/test/java/com/insight/learning/platabank/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insight.learning.platabank.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
