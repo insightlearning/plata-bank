12361196820e0f581a4f827bfb2d7bc2294684b9
Diego Lucas <diego.lucasilva@gmail.com>
2019-10-22 22:56:55 -0300

    chore(customer-service): Exception when customer not found

--- customer-service/src/main/java/com/insight/learning/platabank/customerservice/service/CustomerService.java
+++ customer-service/src/main/java/com/insight/learning/platabank/customerservice/service/CustomerService.java
@@ -3,10 +3,9 @@ package com.insight.learning.platabank.customerservice.service;
 import com.insight.learning.platabank.customerservice.domain.Customer;
 import com.insight.learning.platabank.customerservice.domain.Status;
 import com.insight.learning.platabank.customerservice.repository.CustomerRepository;
-import com.insight.learning.platabank.customerservice.utils.NoContentException;
+import com.insight.learning.platabank.customerservice.utils.CustomerNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,13 +14,6 @@ public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
 
-    public Customer createCustomer(Customer customer){
-        return customerRepository.save(customer);
-    }
-
-    public List<Customer> getAll(){
-        return customerRepository.findAll();
-    }
 
     public Optional<Customer> getCustomer(Long id){
         return customerRepository.findById(id);
@@ -32,7 +24,7 @@ public class CustomerService {
             customer.setStatus(Status.ACTIVE);
             customerRepository.save(customer);
             return customer;
-        }).orElseThrow(NoContentException::new);
+        }).orElseThrow(() -> new CustomerNotFoundException(id));
     }
 
     public void deactivateCustomer(Long id){
@@ -40,7 +32,7 @@ public class CustomerService {
             customer.setStatus(Status.INACTIVE);
             customerRepository.save(customer);
             return customer;
-        }).orElseThrow(NoContentException::new);
+        }).orElseThrow(() -> new CustomerNotFoundException(id));
     }
 
 
