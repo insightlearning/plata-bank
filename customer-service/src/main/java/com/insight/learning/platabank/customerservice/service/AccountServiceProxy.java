7e2fddceab641d7e89b7180e75c0a8e960850b51
Diego Lucas <diego.lucasilva@gmail.com>
2019-10-29 22:42:55 -0300

    reorganize imports

--- customer-service/src/main/java/com/insight/learning/platabank/customerservice/service/AccountServiceProxy.java
+++ customer-service/src/main/java/com/insight/learning/platabank/customerservice/service/AccountServiceProxy.java
@@ -2,7 +2,6 @@ package com.insight.learning.platabank.customerservice.service;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name="zuul-api-gateway-server")
 @RibbonClient(name="account-service")
