04e29ff7c79b2af10cd78c70a04f67ae1b84a104
Diego Lucas <diego.lucasilva@gmail.com>
2019-10-29 22:41:40 -0300

    new methods and annotation ResponseStatus instead ResponseEntity

--- customer-service/src/main/java/com/insight/learning/platabank/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/insight/learning/platabank/customerservice/controller/CustomerController.java
@@ -4,10 +4,11 @@ import com.insight.learning.platabank.customerservice.domain.Customer;
 import com.insight.learning.platabank.customerservice.dto.CustomerDto;
 import com.insight.learning.platabank.customerservice.mapper.CustomerMapper;
 import com.insight.learning.platabank.customerservice.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/customer")
@@ -21,21 +22,36 @@ public class CustomerController {
         this.customerMapper = customerMapper;
     }
 
+    @GetMapping
+    @ResponseBody
+    public List<CustomerDto> findAll(){
+        List<Customer> customers= customerService.findAll();
+        return customers.stream()
+                .map(customer -> customerMapper.toCustomerDto(customer))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public CustomerDto findById(@PathVariable Long id){
+        return customerMapper.toCustomerDto(customerService.findById(id));
+    }
+
     @PostMapping
-    public ResponseEntity<CustomerDto> create(@RequestBody CustomerDto customerDto){
-        customerService.save(customerMapper.toCustomer(customerDto));
-        return ResponseEntity.status(HttpStatus.CREATED).body(customerDto);
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerDto create(@RequestBody CustomerDto customerDto){
+        Customer customer = customerService.save(customerMapper.toCustomer(customerDto)).get();
+        return customerMapper.toCustomerDto(customer);
     }
 
     @PatchMapping("/activate/{id}")
-    public ResponseEntity activateCustomer(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void activateCustomer(@PathVariable Long id){
         customerService.activateCustomer(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @PatchMapping("/deactivate/{id}")
-    public ResponseEntity deactivateCustomer(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deactivateCustomer(@PathVariable Long id){
         customerService.deactivateCustomer(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
