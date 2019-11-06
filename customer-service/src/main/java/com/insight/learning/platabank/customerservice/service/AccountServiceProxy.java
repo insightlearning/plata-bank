65778becffb8b081ad2dc98a754beba5df22338f
Diego Lucas <diego.lucasilva@gmail.com>
2019-11-05 22:11:26 -0300

    account service proxy

--- customer-service/src/main/java/com/insight/learning/platabank/customerservice/service/AccountServiceProxy.java
+++ customer-service/src/main/java/com/insight/learning/platabank/customerservice/service/AccountServiceProxy.java
@@ -1,11 +1,20 @@
 package com.insight.learning.platabank.customerservice.service;
 
+import com.insight.learning.platabank.customerservice.domain.Customer;
+import feign.Headers;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name="zuul-api-gateway-server")
 @RibbonClient(name="account-service")
 public interface AccountServiceProxy {
 
 
+    @PostMapping("/account-service/account/create")
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json"
+    })
+    public void createAccount(Customer customer);
 }
