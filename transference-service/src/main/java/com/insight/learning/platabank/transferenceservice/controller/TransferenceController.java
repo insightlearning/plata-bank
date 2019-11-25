02ecc37062b30318eaff6efb5eafc988ed4c534b
kaique.leao <kaiqueleaodeveloper@gmail.com>
2019-11-25 10:40:17 -0300

    chore: fix controller implementation

--- transference-service/src/main/java/com/insight/learning/platabank/transferenceservice/controller/TransferenceController.java
+++ transference-service/src/main/java/com/insight/learning/platabank/transferenceservice/controller/TransferenceController.java
@@ -6,11 +6,7 @@ import com.insight.learning.platabank.transferenceservice.factory.TransferenceFa
 import com.insight.learning.platabank.transferenceservice.mapper.TransferenceMapper;
 import com.insight.learning.platabank.transferenceservice.service.TransferenceService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -28,7 +24,8 @@ public class TransferenceController {
     }
 
     @PostMapping
-    public ResponseEntity<TransferenceDTO> doTransference(@RequestBody TransferenceDTO transferenceDTO) {
+    @ResponseStatus(HttpStatus.OK)
+    public TransferenceDTO doTransference(@RequestBody TransferenceDTO transferenceDTO) {
 
         TransferenceService transferenceService = transferenceFactory.getTransference(transferenceDTO.getTransferType());
         Optional<Transference> transference = transferenceService
@@ -36,6 +33,6 @@ public class TransferenceController {
                         transferenceMapper.toTransference(transferenceDTO)
                 );
 
-        return new ResponseEntity<TransferenceDTO>(transferenceMapper.toTransferenceDTO(transference.get()), HttpStatus.OK);
+        return transferenceMapper.toTransferenceDTO(transference.get());
     }
 }
